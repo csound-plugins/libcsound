c23a9f43729434e4809ccd8c129e02de941a9df1
gesellkammer <eduardo.moguillansky@gmail.com>
2026-09-14 00:16:17 +0200

    fix api6

--- libcsound/api6.py
+++ libcsound/api6.py
@@ -443,7 +443,7 @@ CSOUND_VAR_CHANNEL = 5
 if not BUILDING_DOCS:
     from . import _dll
     import ctypes.util
-    libcsound, libcsoundpath, opcodedir = _dll.csoundDLL()
+    libcsound, libcsoundpath = _dll.csoundDLL()
     if sys.platform.startswith('linux'):
         libcspt = ct.CDLL("libcsnd6.so")
     elif sys.platform.startswith('win'):
